562bb0b4923a7367761d4544954f54c80fcbf6a6
leikareipa <drilledicecream@gmail.com>
2019-04-05 05:49:24 +0300

    RGEO: Add script for building with Open Watcom

---
+++ modules/rgeo/build_rgeo_owc.sh
@@ -0,0 +1,14 @@
+# Build RGEO for DOS using Open Watcom C/C++ in Linux.
+
+OW_PATH=~/compi/watcom
+OUTPUT_EXE_FILENAME=../bin/rgeo.exe
+BUILD_OPTIONS="-bcl=dos -cc -ml -oneatx -oh -ei -zp2 -4 -fpi87 -sg -k16384 -fe=$OUTPUT_EXE_FILENAME"
+
+cd src
+export WATCOM=$OW_PATH
+export INCLUDE=$WATCOM/h
+PATH=$PATH:$OW_PATH/binl
+wcl $BUILD_OPTIONS *.c
+
+rm *.o
+rm *.err
