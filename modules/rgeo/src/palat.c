af57a155e95170f842b378b768bd4071bb784b1c
leikareipa <drilledicecream@gmail.com>
2019-04-10 20:35:06 +0300

    RGEO: Add getter for individual PALA textures

--- modules/rgeo/src/palat.c
+++ modules/rgeo/src/palat.c
@@ -18,15 +18,23 @@ static u8* PALAT_DATA = NULL;
  * array will hold no more than this number of PALA textures.*/
 #define MAX_NUM_PALAS 253
 
-#define PALA_WIDTH 16
-#define PALA_HEIGHT 16
-const uint NUM_PIXELS_IN_PALA = (PALA_WIDTH * PALA_HEIGHT);
+const uint NUM_PIXELS_IN_PALA = (KP_PALA_WIDTH * KP_PALA_HEIGHT);
 
 void kp_release_palat_data(void)
 {
     free(PALAT_DATA);
+    PALAT_DATA = NULL;
 
     return;
+}
+
+/* Returns a pointer to the master PALAT data such that the subsequent n bytes
+ * are the pixels of the given PALA texture (0-indexed).*/
+const u8* kp_pala(const uint palaIdx)
+{
+    k_assert((palaIdx < MAX_NUM_PALAS), "Attempting to access a PALA texture out of bounds.");
+
+    return &PALAT_DATA[palaIdx * NUM_PIXELS_IN_PALA];
 }
 
 /* Loads the PALAT data from the given RallySportED project file.
@@ -42,23 +50,38 @@ void kp_load_palat_data(const file_handle projFileHandle)
 {
     u32 dataLen = 0; /* Type must be 4 bytes.*/
     u32 skipNumBytes = 0;
-    const uint maxDataLen = (MAX_NUM_PALAS * NUM_PIXELS_IN_PALA);
 
     k_assert((PALAT_DATA == NULL), "Was asked to re-load PALAT data.");
 
-    kf_read_bytes((u8*)&dataLen, 4, projFileHandle);
-    if (dataLen > maxDataLen)
+    /* Get the size of the PALAT data.*/
     {
-        skipNumBytes = (dataLen - maxDataLen);
-        dataLen = maxDataLen;
+        const u32 maxDataLen = (MAX_NUM_PALAS * NUM_PIXELS_IN_PALA);
+        
+        kf_read_bytes((u8*)&dataLen, 4, projFileHandle);
+
+        /* Sanity check. We expect there to never be more than 256 PALA textures
+         * in Rally-Sport's PALAT files.*/
+        k_assert((dataLen <= (256ul * NUM_PIXELS_IN_PALA)), "Detected an invalid PALAT data length in the project file.");
+
+        if (dataLen > maxDataLen)
+        {
+            skipNumBytes = (dataLen - maxDataLen);
+            dataLen = maxDataLen;
+        }
     }
 
-    PALAT_DATA = (u8*)malloc(dataLen);
-    kf_read_bytes(PALAT_DATA, dataLen, projFileHandle);
-    DEBUG(("Read %u bytes of PALAT data into null.", dataLen));
+    /* Read the PALAT data from disk to memory.*/
+    {
+        k_assert(((dataLen % NUM_PIXELS_IN_PALA) == 0), "Detected incomplete PALAT data in the project file.");
 
-    /* Guarantee that the file's cursor will be positioned one byte past the
-     * end of the PALAT data.*/
+        PALAT_DATA = (u8*)malloc(dataLen);
+        kf_read_bytes(PALAT_DATA, dataLen, projFileHandle);
+
+        DEBUG(("Read %u bytes of PALAT data into null.", dataLen));
+    }
+
+    /* Guarantee that the file cursor will be positioned at the byte immediately
+     * following the end of the PALAT data.*/
     if (skipNumBytes) kf_jump(skipNumBytes, projFileHandle);
 
     return;
--- modules/rgeo/src/palat.h
+++ modules/rgeo/src/palat.h
@@ -3,7 +3,14 @@
 
 #include "types.h"
 
+/* A PALA texture's resolution.*/
+#define KP_PALA_WIDTH 16
+#define KP_PALA_HEIGHT 16
+
 void kp_load_palat_data(const file_handle projFileHandle);
 void kp_release_palat_data(void);
+
+/* Getters.*/
+const u8* kp_pala(const uint palaIdx);
 
 #endif
